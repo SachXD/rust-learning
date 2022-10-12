c04349b0dfa221d5e1eeff48496c551699b08f13
Ananth Prathap <ananthbiji@gmail.com>
2022-10-12 19:45:52 +0530

    1st-program-successfully-executed

---
+++ files/hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa.d
@@ -0,0 +1,5 @@
+/home/hashirama/git-clones/rust-leaning-in-public/files/hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa: src/main.rs
+
+/home/hashirama/git-clones/rust-leaning-in-public/files/hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa.d: src/main.rs
+
+src/main.rs:
---
+++ files/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/hashirama/git-clones/rust-leaning-in-public/files/hello-cargo/target/debug/hello-cargo: /home/hashirama/git-clones/rust-leaning-in-public/files/hello-cargo/src/main.rs
