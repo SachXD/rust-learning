2692f3fe12cc5da05c12ba1abe9c4675071daf8d
Ananth Prathap <ananthbiji@gmail.com>
2022-10-13 19:58:19 +0530

    added-a-few-features-like-continious-multiple-guessing-quitting-after-right-guess

--- files/guess_game/src/main.rs
+++ files/guess_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main(){
@@ -17,5 +18,20 @@ fn main(){
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+
+    };
+
     println!("You guessed : {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
 }
