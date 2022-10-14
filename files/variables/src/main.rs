e3043e353a4e1a6264c5858971a71131068d4a70
Ananth Prathap <ananthbiji@gmail.com>
2022-10-14 23:56:14 +0530

    learned-shadowing,difference-between-variables-constants-in-terms-of-mutability

--- files/variables/src/main.rs
+++ files/variables/src/main.rs
@@ -4,5 +4,14 @@ fn main() {
     x = 6;
     println!("The value of x is: {}",x);
 
-    const MAX_POINTS: u32 = 100_00;
+    const MAX_POINTS: u32 = 100_00; //these are constants which are always immutable
+    
+    let spaces = "  ";
+    let spaces = space.len(); //this wont be an error since we are declare another variable space
+                             //which is different from the other which is a string.
+
+
+    let mut spaces = "  ";  //meanwhile this can cause a error since you declared it as a mutable
+    spaces = spaces.len(); //we can cannot change a string type to int (obviously!)
+                            
 }
