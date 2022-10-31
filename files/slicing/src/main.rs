12ec0e36846045cb295f37197daa721f3232c7bb
Ananth Prathap <ananthbiji@gmail.com>
2022-10-31 17:32:46 +0530

    slicing-strings

---
+++ files/slicing/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut s = String::from("Senju");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if (item == b' ') {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
+}
